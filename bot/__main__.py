cae55b330fb9b52ff98b618fe8f433df43229bb5
hammzx <95368225+hammzx@users.noreply.github.com>
2022-02-24 09:11:11 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,7 @@ def start(update, context):
         start_string = f'''
 𝗧𝗵𝗶𝘀 𝗯𝗼𝘁 𝗰𝗮𝗻 𝗺𝗶𝗿𝗿𝗼𝗿 𝗮𝗹𝗹 𝘆𝗼𝘂𝗿 𝗹𝗶𝗻𝗸𝘀 𝘁𝗼 𝗚𝗼𝗼𝗴𝗹𝗲 𝗗𝗿𝗶𝘃𝗲!
 𝗧𝘆𝗽𝗲 /{BotCommands.HelpCommand} 𝘁𝗼 𝗴𝗲𝘁 𝗮 𝗹𝗶𝘀𝘁 𝗼𝗳 𝗮𝘃𝗮𝗶𝗹𝗮𝗯𝗹𝗲 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀.
-
-
+'''
         sendMarkup(start_string, context.bot, update, reply_markup)
     else:
         sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update, reply_markup)
